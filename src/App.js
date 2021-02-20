e9af44d84009b182f45919c5f0fb4a7ef9896bd5
DevalPanchal2401 <devalp2401@gmail.com>
2021-02-19 21:08:51 -0500

    New implementation initial commit

--- src/App.js
+++ src/App.js
@@ -1,76 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import Pokemons from './Components/Pokemons';
-import Page from './Components/Page';
 import axios from 'axios';
 
 
 function App() {
-  const url = "https://pokeapi.co/api/v2/pokemon/";
+  const url = "https://pokeapi.co/api/v2/pokemon";
 
-  const [pokemon, setPokemon] = useState([]);
-  const [pokemonData, setPokemonData] = useState([]);
-
-  const [currentURL, setCurrentURL] = useState(url);
-  const [newURL, setNewURL] = useState();
-  const [prevURL, setPrevURL] = useState();
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(url);
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [previousPageUrl, setPreviousPageUrl] = useState();
 
   useEffect(() => {
-    setLoading(true);
-    
-    let cancel;
-    axios.get(currentURL, {
-      cancelToken: new axios.CancelToken(c => cancel = c)
+    fetch(url).then(res => res.json())
+      .then((data) => {
+        setLoading(false);
+        setPokemon(data.results);
+        console.log(data);
+      }, (error) => {
+        setLoading(false);
+        setError(error);
+      })
+  }, [])
 
-    }).then(res => {
-      console.log(res.data);
-      const pokemonDataUrls = [];
 
-      setLoading(false);
-      setNewURL(res.data.next);
-      setPrevURL(res.data.previous);
-      setPokemon(res.data.results.map(p => p.name));
-
-      pokemonDataUrls.push(res.data.results.map(u => u.url));
-
-      setPokemonData(res.data.results.map(d => d.url));
-    });
-    return () => {
-      cancel()
-    }
-  }, [currentURL]);
-
-  function nextPage() {
-    setCurrentURL(newURL);
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (loading) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div>
+        <div>
+          {pokemon.map(p => (
+            <div key={p.id}>{p.name}</div>
+          ))}
+        </div>
+      </div>
+    );
   }
-
-  function prevPage() {
-    setCurrentURL(prevURL);
-  }
-
-  if (loading) {
-    return "Loading ...";
-  }
-
-  return (
-    <div>
-      <Pokemons pokemon={ pokemon } pokemonData={ pokemonData }/>
-      <Page nextPage={newURL ? nextPage : null} prevPage={prevURL ? prevPage : null }/>
-    </div>
-  );
 }
-// const [pokemon, setPokemon] = useState([]);
-//   const [pokemonData, setPokemonData] = useState([]);
 
-//   async function getPokemon() {
-//     const array = [];
-//     try {
-//       const url = "https://pokeapi.co/api/v2/pokemon/";
-//       const res = await axios.get(url);
-//       console.log(res.data);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
 
 export default App;
