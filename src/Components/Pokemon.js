ef9787595e06a3fa986b5993100af5e85ff8c0e6
DevalPanchal2401 <devalp2401@gmail.com>
2021-03-15 22:20:55 -0400

    changed images to official artwork

--- src/Components/Pokemon.js
+++ src/Components/Pokemon.js
@@ -8,8 +8,8 @@ export default function Pokemon({ pokemon }) {
             <h6 className="card-subtitle mb-2 text-muted">Id: {pokemon.id}</h6>  
             <h6 className="card-subtitle mb-2 text-muted">Height: {pokemon.height}</h6>  
             <h6 className="card-subtitle mb-2 text-muted">Weight: {pokemon.weight}</h6>  
-            <img src={pokemon.sprites['front_default']} alt={pokemon.name} />
-            <img src={pokemon.sprites['back_default']} alt={pokemon.name} />                       
+            <img src={pokemon.sprites.other['official-artwork']['front_default']} alt={pokemon.name} />
+            {/* <img src={pokemon.sprites['back_default']} alt={pokemon.name} />                        */}
           </div>
         </div>
       )
