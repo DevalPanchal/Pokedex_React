0597a0209f5bc36c2e6b8d3c0cba7145ffbb6f7c
DevalPanchal2401 <devalp2401@gmail.com>
2021-03-19 21:04:39 -0400

    Added new implementation

--- src/App.js
+++ src/App.js
@@ -1,93 +1,52 @@
-import React from 'react';
-import { Component } from 'react';
-import Pokemon from './Components/Pokemon'
-import './stylesheet/style.css'
+import React, { useState, useEffect } from "react";
+import Pokemon from "./Components/Pokemon";
+import "./stylesheet/style.css";
 
 const url = "https://pokeapi.co/api/v2/pokemon/";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUrl: url,
-      nextPageUrl: "",
-      previousPageUrl: "",
-      pokemonList: [],
-      pokemonData: [],
-    }
-    this.handleNextPageClick = this.handleNextPageClick.bind(this);
-    this.handlePreviousPageClick = this.handlePreviousPageClick.bind(this);
-  }
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPageUrl, setCurrentPageUrl] = useState(url);
+  const [nextPageUrl, setNextPageUrl] = useState("");
+  const [previousPageUrl, setpreviousPageUrl] = useState("");
 
-  componentDidMount() {
-    this.getPokemon(this.state.currentUrl);
-  }
+  useEffect(() => {
+    fetchPokemon(currentPageUrl);
+  }, [currentPageUrl]);
 
-  componentWillUnmount() { 
-  }
+  async function fetchPokemon(url) {
+    setLoading(false);
+    let response = await fetch(url);
+    let data = await response.json();
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.currentUrl !== this.state.currentUrl) {
-      this.getPokemon(this.state.currentUrl);
+    if (data) {
+      setPokemon(data.results);
+      setNextPageUrl(data.next);
+      setpreviousPageUrl(data.previous);
+      
     }
   }
 
-  async getPokemon(url) {
-    try {
-      let response = await fetch(url);
-      let data = await response.json();
-
-      if (data) {
-        this.setState({ pokemonList: data.results, nextPageUrl: data.next, previousPageUrl: data.previous }, () => {
-          this.state.pokemonList.map(async pokemon => {
-            try {
-              let response = await fetch(pokemon.url);
-              let data = await response.json();
-
-              if (data) {
-                var temp = this.state.pokemonData;
-                temp.push(data);
-                this.setState({ pokemonData: temp })
-              }
-            } catch (error) {
-              console.error(error);
-            }
-
-          })
-        })
-      }
-    } catch (error) {
-      console.error(error);
-    }
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl);
   }
 
-  handleNextPageClick = () => {
-    this.setState({ currentUrl: this.state.nextPageUrl });
+  function goToPreviousPage() {
+    setCurrentPageUrl(previousPageUrl);
   }
 
-  handlePreviousPageClick = () => {
-    this.setState({ currentUrl: this.state.previousPageUrl });
-  }
+  if (loading) return <h1>loading...</h1>;
 
-  render() {
-    const { pokemonData, previousPageUrl, nextPageUrl } = this.state;
 
-    const displayPokemon = pokemonData.map((pokemon, index) => {
-      return (
-        <Pokemon pokemon={pokemon} key={index}/>
-      );
-    });
 
-    return (
-      <div className="pokemons">
-        <div className="pokemon-display">
-          { displayPokemon }
-        </div>
-        { previousPageUrl && <button onClick={this.handlePreviousPageClick}>Previous</button>}
-        { nextPageUrl && <button onClick={this.handleNextPageClick}>Next</button>}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Pokemon pokemon={ pokemon } />
+      {previousPageUrl && <button onClick={goToPreviousPage}>previous</button>}
+      {nextPageUrl && <button onClick={goToNextPage}>next</button>}
+    </div>
+  );
 }
 
 export default App;
